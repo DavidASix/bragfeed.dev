e47baaae4f967a888c4cfbc18a61f466fad5f5ed
davidasix <david@davidasix.com>
2025-08-21 11:19:43 -0400

    Properly seed UUID's

--- src/scripts/seed/business_stats.ts
+++ src/scripts/seed/business_stats.ts
@@ -1,11 +1,13 @@
 import { db } from "@/schema/db";
 import { business_stats } from "@/schema/schema";
+ 
+import { businessId } from "./businesses";
 
 export async function up() {
   console.log("Seeding business_stats table...");
 
   await db.insert(business_stats).values({
-    business_id: 1,
+    business_id: businessId,
     review_count: 218,
     review_score: 5,
     created_at: new Date("2025-07-27T04:22:08.603Z"),
--- src/scripts/seed/events.ts
+++ src/scripts/seed/events.ts
@@ -1,6 +1,7 @@
 import { db } from "@/schema/db";
 import { events } from "@/schema/schema";
 
+import { businessId } from "./businesses";
 export async function up() {
   console.log("Seeding events table...");
 
@@ -8,13 +9,13 @@ export async function up() {
     {
       event: "update_reviews",
       user_id: "6506bac5-e63a-4fa3-b9c9-a94ab5a549fc",
-      metadata: { business_id: 1 },
+      metadata: { business_id: businessId },
       timestamp: new Date("2025-07-27T04:22:09.499Z"),
     },
     {
       event: "update_stats",
       user_id: "6506bac5-e63a-4fa3-b9c9-a94ab5a549fc",
-      metadata: { business_id: 1 },
+      metadata: { business_id: businessId },
       timestamp: new Date("2025-07-27T04:22:09.502Z"),
     },
   ]);
