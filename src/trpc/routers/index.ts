7410b0dd7250b68928e3debc942ef86f80e71c89
davidasix <david@davidasix.com>
2026-09-02 19:42:23 -0400

    Add service health router

--- src/trpc/routers/index.ts
+++ src/trpc/routers/index.ts
@@ -1,12 +1,14 @@
 import { createCallerFactory, router } from "..";
 import { dashboardRouter } from "./dashboard";
 import { googleRouter } from "./google";
+import { healthRouter } from "./health";
 import { purchasesRouter } from "./purchases";
 import { securityRouter } from "./security";
 
 export const appRouter = router({
   dashboard: dashboardRouter,
   google: googleRouter,
+  health: healthRouter,
   purchases: purchasesRouter,
   security: securityRouter,
 });
